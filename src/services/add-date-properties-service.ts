5d1a0286ea752f0fc50444c40f5753975a588a34
tadashi-aikawa <syou.maman@gmail.com>
2024-01-11 22:02:43 +0900

    [リファクタリング] サービスをすべて別ファイルに切り出し

---
+++ src/services/add-date-properties-service.ts
@@ -0,0 +1,38 @@
+import { now } from "src/lib/helpers/datetimes";
+import { appendTextToFile, loadFileContent } from "src/lib/helpers/entries";
+import { setOnCreateFileEvent } from "src/lib/helpers/events";
+import { Service } from "src/services";
+
+/**
+ * 新しくファイルを開いたときにテンプレのテキストを差し込むサービスです
+ * 既にテキストが存在する場合は何もしません
+ */
+export class AddDatePropertiesService implements Service {
+  name = "Add date properties";
+  unsetHandler!: () => void;
+
+  onLayoutReady(): void {
+    this.unsetHandler = setOnCreateFileEvent(async (file) => {
+      const content = await loadFileContent(file.path);
+      if (content) {
+        // テンプレ付きのコンテンツの場合は何もしない
+        return;
+      }
+
+      const today = now("YYYY-MM-DD");
+      await appendTextToFile(
+        file.path,
+        `
+---
+created: ${today}
+updated: ${today}
+---
+      `.trim()
+      );
+    });
+  }
+
+  onunload(): void {
+    this.unsetHandler();
+  }
+}
---
+++ src/services/add-date-properties-to-head-service.ts
@@ -0,0 +1,95 @@
+import { getActiveFilePath } from "src/lib/helpers/entries";
+import {
+  setOnFileOpenEvent,
+  setOnPropertiesChangedEvent,
+} from "src/lib/helpers/events";
+import { getPropertiesByPath } from "src/lib/helpers/properties";
+import {
+  insertElementBeforeHeader,
+  removeElementsFromContainer,
+} from "src/lib/helpers/ui";
+import { Service } from "src/services";
+
+/**
+ * 新しくファイルを開いたときにcreated/updatesプロパティを差し込むサービスです
+ * 既にテキストが存在する場合は何もしません
+ */
+export class AddDatePropertiesToHeadService implements Service {
+  name = "Add date properties to head";
+  className = "additional-date-properties";
+
+  unsetFileOpenHandler!: () => void;
+  unsetPropertiesChangedEventRef!: () => void;
+
+  onload() {
+    this.unsetFileOpenHandler = setOnFileOpenEvent((file) => {
+      if (!file) {
+        return;
+      }
+
+      this.removeDatePropertiesElements();
+      this.addDatePropertiesElement(file.path);
+    });
+
+    this.unsetPropertiesChangedEventRef = setOnPropertiesChangedEvent(
+      (file, _, cache) => {
+        this.removeDatePropertiesElements();
+        if (cache.frontmatter?.created && cache.frontmatter?.updated) {
+          this.addDatePropertiesElement(file.path);
+        }
+      }
+    );
+
+    // 初回はイベントが発生しないので
+    const path = getActiveFilePath();
+    if (path != null) {
+      this.addDatePropertiesElement(path);
+    }
+  }
+
+  onunload() {
+    this.unsetFileOpenHandler();
+    this.unsetPropertiesChangedEventRef();
+    this.removeDatePropertiesElements();
+  }
+
+  /**
+   * 日付ボタンの要素を作成します
+   * @param title (ex: 作成: 2023-10-09)
+   */
+  createDateButton(title: string): HTMLElement {
+    return createDiv({
+      text: title,
+      cls: "additional-date-properties__date-button",
+    });
+  }
+
+  /**
+   * ファイルが表示されているViewに日付プロパティ要素を追加します
+   * @param path 追加するViewに表示されているファイルのpath
+   */
+  addDatePropertiesElement(path: string): void {
+    const properties = getPropertiesByPath(path);
+    if (!properties) {
+      return;
+    }
+
+    const { created, updated } = properties;
+    if (!(created && updated)) {
+      return;
+    }
+
+    const datePropertiesEl = createDiv({ cls: this.className });
+    datePropertiesEl.appendChild(this.createDateButton(`作成日: ${created}`));
+    datePropertiesEl.appendChild(this.createDateButton(`更新日: ${updated}`));
+    insertElementBeforeHeader(datePropertiesEl);
+  }
+
+  /**
+   * ファイルが表示されているViewから日付プロパティ要素を削除します
+   * @param path 削除するViewに表示されているファイルのpath
+   */
+  removeDatePropertiesElements(): void {
+    removeElementsFromContainer(`.${this.className}`);
+  }
+}
