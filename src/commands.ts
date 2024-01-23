c4c019998756f0e84b749d61756702b09970f766
tadashi-aikawa <syou.maman@gmail.com>
2024-01-23 17:22:36 +0900

    Copy url propertyコマンドの追加

--- src/commands.ts
+++ src/commands.ts
@@ -29,6 +29,7 @@ import { showAnotherCommandPalette } from "./commands/show-another-command-palet
 import { copyAsConfluence } from "./commands/copy-as-confluence";
 import { addUrlProperty } from "./commands/add-url-property";
 import { openPropertyUrl } from "./commands/open-property-url";
+import { copyUrlProperty } from "./commands/copy-url-property";
 
 // INFO: _settingsは使ってないけど、設定を使いたい場合はここで渡すべし
 export function createCommands(_settings: PluginSettings): Command[] {
@@ -144,6 +145,12 @@ export function createCommands(_settings: PluginSettings): Command[] {
       name: "Copy active file full path",
       kind: "file",
       executor: copyActiveFileFullPath,
+      hideOnCommandList: true,
+    },
+    {
+      name: "Copy url property",
+      kind: "editor",
+      executor: copyUrlProperty,
       hideOnCommandList: true,
     },
     {
---
+++ src/commands/copy-url-property.ts
@@ -0,0 +1,15 @@
+import { getActiveFileProperties } from "src/lib/helpers/properties";
+import { copyToClipboard, notify } from "src/lib/helpers/ui";
+
+/**
+ * URLプロパティの値をクリップボードにコピーします
+ */
+export async function copyUrlProperty() {
+  const url = getActiveFileProperties()?.url;
+  if (!url) {
+    return;
+  }
+
+  await copyToClipboard(url);
+  notify("👍URLをコピーしました", 5000);
+}
