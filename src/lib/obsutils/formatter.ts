f085067271ffda676cf985ab4c541ea4138d8549
tadashi-aikawa <syou.maman@gmail.com>
2025-05-14 12:05:01 +0900

    fix(Format): コードブロック内でフォーマットがかかってしまう

--- src/lib/obsutils/formatter.ts
+++ src/lib/obsutils/formatter.ts
@@ -11,17 +11,33 @@ interface TextReplacement {
 
 const isEmptyLine = (x: string) => x.trim() === "";
 
+function toText(line: string): string {
+  if (line.startsWith("# ")) {
+    return "\n\n";
+  }
+  if (line.startsWith("## ")) {
+    return "\n";
+  }
+  return "";
+}
+
 /**
  * @param cursorLineNo カーソル行番号(1~)
  */
 export function* formatLineBreaks(markdown: string): Iterable<TextReplacement> {
   let firstEmptyI = null;
+  let inCodeBlock = false;
   const lines = markdown.split("\n");
 
   for (let i = 0; i < lines.length; i++) {
     const line = lines[i];
 
-    if (isEmptyLine(line)) {
+    if (line.startsWith("```")) {
+      inCodeBlock = !inCodeBlock;
+    }
+
+    // コードブロック以外の空行はformat対象になる
+    if (isEmptyLine(line) && !inCodeBlock) {
       if (firstEmptyI == null) {
         firstEmptyI = i;
       }
@@ -35,8 +51,9 @@ export function* formatLineBreaks(markdown: string): Iterable<TextReplacement> {
     yield {
       from: { line: firstEmptyI, ch: 0 },
       to: { line: i - 1, ch: 0 },
-      text: line.startsWith("# ") ? "\n\n" : line.startsWith("## ") ? "\n" : "",
+      text: toText(line),
     };
+
     firstEmptyI = null;
   }
 }
