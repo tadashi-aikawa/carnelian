e1d92c0fb8aabf22b4aeb3500efdc45fd6b99f15
tadashi-aikawa <syou.maman@gmail.com>
2025-08-26 08:29:48 +0900

    feat(service): linter設定がtrueの場合のみLint serviceを有効にする

--- src/services.ts
+++ src/services.ts
@@ -12,11 +12,11 @@ export interface Service {
 }
 
 // INFO: _settingsは使ってないけど、設定を使いたい場合はここで渡すべし
-export function createServices(_settings: PluginSettings): Service[] {
+export function createServices(settings: PluginSettings): Service[] {
   return [
     new AddDatePropertiesService(),
-    new LintService(),
+    settings.linter ? new LintService() : [],
     new FormatService(),
     new AddPropertiesToHeadService(),
-  ];
+  ].flat();
 }
