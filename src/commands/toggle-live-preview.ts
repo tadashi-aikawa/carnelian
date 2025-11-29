053414359fdfa8c38cd9d34d7dbe60ba1ce629b6
tadashi-aikawa <syou.maman@gmail.com>
2025-11-30 06:27:44 +0900

    feat(toggle-live-preview): 切り替え後にフォーカスが外れる問題に対応

--- src/commands/toggle-live-preview.ts
+++ src/commands/toggle-live-preview.ts
@@ -1,4 +1,7 @@
-import { setLivePreview } from "src/lib/helpers/editors/basic";
+import {
+  getActiveCMEditor,
+  setLivePreview,
+} from "src/lib/helpers/editors/basic";
 import { getAllMarkdownLeaves } from "src/lib/helpers/leaves";
 import { toggleDefaultEditingMode } from "src/lib/helpers/settings";
 
@@ -6,8 +9,13 @@ import { toggleDefaultEditingMode } from "src/lib/helpers/settings";
  * LivePreviewモードを切り替えます
  */
 export function toggleLivePreview() {
+  const activeCM = getActiveCMEditor();
+
   const nextDefault = toggleDefaultEditingMode() === "livePreview";
   for (const l of getAllMarkdownLeaves()) {
     setLivePreview(l, nextDefault);
   }
+
+  // 切り替え後にフォーカスが失われる場合の対応
+  activeCM?.focus();
 }
